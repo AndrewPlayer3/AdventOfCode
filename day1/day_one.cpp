15b368b7761f962e1c0e8658bc5cc5115ce6e593
Andrew Player <andrewplayer3@gmail.com>
2019-12-03 07:37:37 -0900

    initial files + day1

---
+++ day1/day_one.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <fstream>
+
+int accumulate(int& total, int& current) {
+    int i = current / 3;
+    int j = i - 2;
+    if(j <= 0) return total;
+    total += j;
+    return accumulate(total, j);
+}
+
+int sum(std::vector<int> data) {
+    int total = 0;
+    for(int i : data) {
+        int temp_total = 0;
+        total += accumulate(temp_total, i);
+    }
+    return total;
+}
+ 
+int main() {
+
+    std::ifstream file("input.txt");
+    std::string line = "";
+    std::vector<int> data;
+    while(std::getline(file, line)) {
+        int i;
+        std::istringstream input(line);
+        if(input >> i) {
+            data.push_back(i);
+        }
+    }
+    int s = sum(data);
+    std::cout << s << std::endl;
+    return 0;
+}

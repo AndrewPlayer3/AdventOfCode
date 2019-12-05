aca43c557ce2971b75e04089b49465dcacf6240a
Andrew Player <andrewplayer3@gmail.com>
2019-12-05 01:35:23 -0900

    finished day5

--- cplusplus/day5/day_five.cpp
+++ cplusplus/day5/day_five.cpp
@@ -12,32 +12,46 @@ vector<int> run(vector<int> mem) {
         int A  = ((mem[i] % 1000) - OP) / 100;
         int B  = ((mem[i] % 10000) - A)  / 1000;
         int C  = ((mem[i] % 100000) - B)  / 10000; // Should always be ZERO
+        int a_param; int b_param;
+        if(i+1 < mem.size()) {
+            if(A == 0) a_param = mem[i+1];
+            else a_param = i+1;
+        }
+        if(i+2 < mem.size()) {
+            if(B == 0) b_param = mem[i+2];
+            else b_param = i+2;
+        }        
         if(OP == 1)  {
-            if     (A == 0 && B == 0) mem[mem[i+3]] = mem[mem[i+1]] + mem[mem[i+2]];
-            else if(A == 1 && B == 0) mem[mem[i+3]] = mem[    i+1 ] + mem[mem[i+2]];
-            else if(A == 0 && B == 1) mem[mem[i+3]] = mem[mem[i+1]] + mem[    i+2 ];
-            else if(A == 1 && B == 1) mem[mem[i+3]] = mem[    i+1 ] + mem[    i+2 ];
+            mem[mem[i+3]] = mem[a_param] + mem[b_param];
             i+=4;
         } else if(OP == 2)  {
-            if     (A == 0 && B == 0) mem[mem[i+3]] = mem[mem[i+1]] * mem[mem[i+2]];
-            else if(A == 1 && B == 0) mem[mem[i+3]] = mem[    i+1 ] * mem[mem[i+2]];
-            else if(A == 0 && B == 1) mem[mem[i+3]] = mem[mem[i+1]] * mem[    i+2 ];
-            else if(A == 1 && B == 1) mem[mem[i+3]] = mem[    i+1 ] * mem[    i+2 ];
+            mem[mem[i+3]] = mem[a_param] * mem[b_param];
             i+=4;
-        }
-        else if(OP == 3)  {
+        } else if(OP == 3)  {
             int input;
             cout << "Enter an int: ";
             cin >> input;
             mem[mem[i+1]] = input;
             i+=2;
-        }
-        else if(OP == 4 ) {
+        } else if(OP == 4 ) {
             if(A == 0) cout << mem[mem[i+1]] << endl;  
             else       cout << mem[    i+1 ] << endl;
             i+=2;
-        }
-        else if(OP == 99) {
+        } else if(OP == 5) {
+            if(mem[a_param] != 0) i = mem[b_param]; 
+            else i+=3;
+        } else if(OP == 6) {
+            if(mem[a_param] == 0) i = mem[b_param]; 
+            else i+=3;
+        } else if(OP == 7) {
+            if(mem[a_param] < mem[b_param]) mem[mem[i+3]] = 1; 
+            else mem[mem[i+3]] = 0;
+            i+=4;
+        } else if(OP == 8) {
+            if(mem[a_param] == mem[b_param]) mem[mem[i+3]] = 1; 
+            else mem[mem[i+3]] = 0;
+            i+=4;
+        } else if(OP == 99) {
             return mem;
         } else {
             i++;
@@ -75,14 +89,22 @@ int main() {
         string temp = "";
         while(pos != line.size()) {
             if(isdigit(line[pos]) || line[pos] == '-') {
-                temp += line[pos++];
-            } else if(line[pos] == ',') {
+                temp += line[pos];
+            }
+            if(line[pos] == ',' || pos == line.size() - 1) {
                 mem.push_back(to_int(temp));
                 temp = "";
-                pos++;
+                pos;
             }
+            ++pos;
         }
     }
+    cout << "[";
+    for(int i = 0; i < mem.size(); i++) {
+        cout << mem[i];
+        if(i != mem.size() - 1) cout << ", ";
+    }
+    cout << "]" << endl;
     vector<int> done = run(mem);
     return 0;
 }

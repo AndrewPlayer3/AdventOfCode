1c22a15fbd0e605b5d2fd41e2b3bd5a9886ff2fb
Andrew Player <asplayer@alaska.edu>
2019-12-05 19:59:31 -0900

    initial day6 cpp file

---
+++ cplusplus/day6/day_six.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+
+
+int main() {
+
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
